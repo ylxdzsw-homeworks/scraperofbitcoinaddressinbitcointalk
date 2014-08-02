f1f81a2f145240d9d39b029888e2b81d168ebe31
ylxdzsw <ylxdzsw@gmail.com>
2014-08-02 13:32:12 +0800

    setup project

---
+++ scraperofbitcoinaddressinbitcointalk/scraperofbitcoinaddressinbitcointalk/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scraperofbitcoinaddressinbitcointalk project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scraperofbitcoinaddressinbitcointalk'
+
+SPIDER_MODULES = ['scraperofbitcoinaddressinbitcointalk.spiders']
+NEWSPIDER_MODULE = 'scraperofbitcoinaddressinbitcointalk.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scraperofbitcoinaddressinbitcointalk (+http://www.yourdomain.com)'
